0bc759271b5910387fcde169f7ec4462870665e7
Genie <genie0320@gmail.com>
2021-11-10 15:05:51 +0900

    Setup react-router-dom 5.2

---
+++ src/pages/About.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque eos temporibus quisquam perferendis voluptas corporis deleniti deserunt, placeat optio officia eum ad. Consectetur, at? Ipsam culpa consectetur eaque autem atque!</p>
+        </div>
+    )
+}
